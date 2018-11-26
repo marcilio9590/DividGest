a31944ef4f7863a9644dff13bce545af9d6aa9e9
Marcilio Cobel <marcilio.c.junior@accenture.com>
2018-11-25 23:14:21 -0300

    inserindo e listando do firebase;

--- src/pages/cartoes/cartoes.ts
+++ src/pages/cartoes/cartoes.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { CartoesProvider } from './../../providers/cartoes/cartoes';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-/**
- * Generated class for the CartoesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,12 +10,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'cartoes.html',
 })
 export class CartoesPage {
+  title: string;
+  form: FormGroup;
+  cartoes: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+
+  constructor(
+    public navCtrl: NavController, public navParams: NavParams,
+    private formBuilder: FormBuilder, private provider: CartoesProvider,
+    private toast: ToastController) {
+    this.cartoes = this.navParams.data.cartoes || {};
+    this.createForm();
+    this.setUpPageTitle();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CartoesPage');
+  private setUpPageTitle() {
+    this.title = this.navParams.data.cartoes ? 'Alteradando Cartão' : 'Novo Cartão';
   }
 
+  createForm() {
+    this.form = this.formBuilder.group({
+      key: [this.cartoes.key],
+      nome: [this.cartoes.nome, Validators.required],
+      vencimento: [this.cartoes.vencimento, Validators.required],
+    })
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.provider.save(this.form.value)
+        .then(() => {
+          this.toast.create({ message: 'Cartão salvo com sucesso.', duration: 3000 }).present();
+        }).catch((e) => {
+          this.toast.create({ message: 'Erro ao salvar cartão.', duration: 3000 }).present();
+          console.log(e);
+        });
+    }
+  }
 }
--- src/providers/cartoes/cartoes.ts
+++ src/providers/cartoes/cartoes.ts
@@ -1,17 +1,45 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 
-/*
-  Generated class for the CartoesProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class CartoesProvider {
+  private BAR = "/";
+  private PATH = 'cartoes';
+  constructor(private db: AngularFireDatabase) { }
 
-  constructor(public http: HttpClient) {
-    console.log('Hello CartoesProvider Provider');
+  getAll() {
+    return this.db.list(this.PATH)
+      .valueChanges();
+  }
+
+  get(key: string) {
+    return this.db.object(this.PATH + this.BAR + key)
+      .valueChanges();
+  }
+
+  save(cartoes: any) {
+    // Update sobrescreve apenas as propriedades passadas.
+    // set sobreescreve todo o objeto da key
+    return new Promise((resolve, reject) => {
+      if (cartoes.key) {
+        // this.db.list(this.PATH)
+        //   .update(cartoes.key, { nome: cartoes.nome, vencimento: cartoes.vencimento })
+        //   .then(() => resolve())
+        //   .catch((e) => reject(e));
+        this.db.object(this.PATH + this.BAR + cartoes.key)
+          .update({ nome: cartoes.nome, vencimento: cartoes.vencimento })
+          .then(() => resolve())
+          .catch((e) => reject(e));
+      } else {
+        this.db.list(this.PATH)
+          .push({ nome: cartoes.nome, vencimento: cartoes.vencimento })
+          .then((result: any) => resolve(result.key));
+      }
+    })
+  }
+
+  remove(key: string) {
+    return this.db.list(this.PATH).remove(key);
   }
 
 }
